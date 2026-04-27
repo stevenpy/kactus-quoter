942ec7e70207136650c58f6ce15aeeb941de26ed
stevenpy <stevenphilbert@gmail.com>
2026-04-28 00:46:38 +0200

    add routes and quote views

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,5 @@
 module ApplicationHelper
+  def euros_from_cents(cents)
+    number_to_currency(cents / 100.0, unit: "€", precision: 2)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -10,5 +10,12 @@ Rails.application.routes.draw do
   # get "service-worker" => "rails/pwa#service_worker", as: :pwa_service_worker
 
   # Defines the root path route ("/")
-  # root "posts#index"
+  root "quotes#index"
+
+  resources :quotes do
+    member do
+      patch :validate
+    end
+    resources :items
+  end
 end
