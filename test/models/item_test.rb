ad38089959d065814ae5806c45f95027f42ea12e
stevenpy <stevenphilbert@gmail.com>
2026-04-29 12:26:54 +0200

    add model tests

--- test/models/item_test.rb
+++ test/models/item_test.rb
@@ -1,7 +1,77 @@
 require "test_helper"
 
 class ItemTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  setup do
+    @quote = Quote.create!(name: "Devis client")
+  end
+
+  test "is valid with valid attributes" do
+    item = @quote.items.build(name: "Design", quantity: 2, unit_price_cents: 10_000, vat_rate: 20)
+
+    assert item.valid?
+  end
+
+  test "requires a name" do
+    item = @quote.items.build(name: nil, quantity: 2, unit_price_cents: 10_000, vat_rate: 20)
+
+    assert_not item.valid?
+    assert_includes item.errors[:name], "can't be blank"
+  end
+
+  test "requires a positive integer quantity" do
+    item = @quote.items.build(name: "Design", quantity: 0, unit_price_cents: 10_000, vat_rate: 20)
+
+    assert_not item.valid?
+    assert_includes item.errors[:quantity], "must be greater than 0"
+  end
+
+  test "requires a non-negative unit price" do
+    item = @quote.items.build(name: "Design", quantity: 2, unit_price_cents: -1, vat_rate: 20)
+
+    assert_not item.valid?
+    assert_includes item.errors[:unit_price_cents], "must be greater than or equal to 0"
+  end
+
+  test "requires a vat rate between 0 and 100" do
+    item = @quote.items.build(name: "Design", quantity: 2, unit_price_cents: 10_000, vat_rate: 101)
+
+    assert_not item.valid?
+    assert_includes item.errors[:vat_rate], "must be less than or equal to 100"
+  end
+
+  test "calculates subtotal, vat amount, and total including vat" do
+    item = @quote.items.build(name: "Design", quantity: 2, unit_price_cents: 10_000, vat_rate: 20)
+
+    assert_equal 20_000, item.subtotal_excl_vat_cents
+    assert_equal 4_000, item.vat_amount_cents
+    assert_equal 24_000, item.total_incl_vat_cents
+  end
+
+  test "cannot be created when quote is validated" do
+    @quote.mark_as_validated!
+    item = @quote.items.build(name: "Design", quantity: 2, unit_price_cents: 10_000, vat_rate: 20)
+
+    assert_not item.save
+    assert_includes item.errors[:base], "Changement impossible sur un devis validé"
+  end
+
+  test "cannot be updated when quote is validated" do
+    item = @quote.items.create!(name: "Design", quantity: 2, unit_price_cents: 10_000, vat_rate: 20)
+    @quote.mark_as_validated!
+
+    item.name = "Nouveau nom"
+
+    assert_not item.save
+    assert_includes item.errors[:base], "Changement impossible sur un devis validé"
+  end
+
+  test "cannot be destroyed when quote is validated" do
+    item = @quote.items.create!(name: "Design", quantity: 2, unit_price_cents: 10_000, vat_rate: 20)
+    @quote.mark_as_validated!
+
+    assert_no_difference "Item.count" do
+      assert_not item.destroy
+    end
+    assert_includes item.errors[:base], "Article non supprimable car devis déjà validé"
+  end
 end
--- test/models/quote_test.rb
+++ test/models/quote_test.rb
@@ -1,7 +1,66 @@
 require "test_helper"
 
 class QuoteTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "is valid with a name" do
+    quote = Quote.new(name: "Devis client")
+
+    assert quote.valid?
+  end
+
+  test "requires a name" do
+    quote = Quote.new(name: nil)
+
+    assert_not quote.valid?
+    assert_includes quote.errors[:name], "can't be blank"
+  end
+
+  test "calculates totals from items" do
+    quote = Quote.create!(name: "Devis client")
+    quote.items.create!(name: "Design", quantity: 2, unit_price_cents: 10_000, vat_rate: 20)
+    quote.items.create!(name: "Development", quantity: 1, unit_price_cents: 15_000, vat_rate: 10)
+
+    assert_equal 35_000, quote.total_excl_vat_cents
+    assert_equal 5_500, quote.total_vat_cents
+    assert_equal 40_500, quote.total_incl_vat_cents
+  end
+
+  test "mark_as_validated updates status and validation timestamp" do
+    quote = Quote.create!(name: "Devis client")
+
+    quote.mark_as_validated!
+
+    assert_predicate quote, :validated?
+    assert_not_nil quote.validated_at
+  end
+
+  test "validated quote cannot change name" do
+    quote = Quote.create!(name: "Devis client")
+    quote.mark_as_validated!
+
+    quote.name = "Nouveau nom"
+
+    assert_not quote.save
+    assert_includes quote.errors[:base], "Devis non modifiable car déjà validé"
+  end
+
+  test "validated quote cannot go back to draft" do
+    quote = Quote.create!(name: "Devis client")
+    quote.mark_as_validated!
+
+    quote.status = :draft
+
+    assert_not quote.save
+    assert_includes quote.errors[:base], "Devis non modifiable car déjà validé"
+    assert_predicate quote.reload, :validated?
+  end
+
+  test "validated quote cannot be destroyed" do
+    quote = Quote.create!(name: "Devis client")
+    quote.mark_as_validated!
+
+    assert_no_difference "Quote.count" do
+      assert_not quote.destroy
+    end
+    assert_includes quote.errors[:base], "Devis non supprimable car déjà validé"
+  end
 end
